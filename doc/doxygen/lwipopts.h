b8bff0fb63c801f4ae5bf96139ef5431b2b7586f
Kjetil Osteras <kjetil.osteras@silabs.com>
2015-12-03 00:02:59 +0100

    Updating doxygen file to be able to generate html documentation with a new version of doxygen

---
+++ doc/doxygen/lwipopts.h
@@ -0,0 +1,36 @@
+#ifndef __LWIP_LWIPOPTS_H__
+#define __LWIP_LWIPOPTS_H__
+
+#define MEMP_OVERFLOW_CHECK             1
+#define MEMP_SANITY_CHECK               1
+#define ETHARP_SUPPORT_VLAN             1
+#define IP_FORWARD                      1
+#define LWIP_AUTOIP                     1
+#define LWIP_DHCP                       1
+#define LWIP_UDPLITE                    1
+#define LWIP_RAW                        1
+#define LWIP_IGMP                       1
+#define LWIP_DNS                        1
+#define LWIP_NETIF_API                  1
+#define LWIP_NETIF_STATUS_CALLBACK      1
+#define LWIP_NETIF_LINK_CALLBACK        1
+#define LWIP_NETIF_REMOVE_CALLBACK      1
+#define LWIP_NETIF_LOOPBACK             1
+#define LWIP_HAVE_SLIPIF                1
+#define LWIP_STATS                      1
+#define LWIP_STATS_DISPLAY              1
+#define MIB2_STATS                      1
+#define PPP_SUPPORT                     1
+#define PPPOE_SUPPORT                   1
+#define PPPOL2TP_SUPPORT                1
+#define PPP_SERVER                      1
+#define LWIP_IPV6                       1
+#define LWIP_IPV6_FORWARD               1
+#define LWIP_IPV6_FRAG                  1
+#define LWIP_IPV6_DHCP6                 1
+#define LWIP_PERF                       1
+#define LWIP_SNMP                       1
+#define LWIP_HTTPD_SSI                  1
+#define LWIP_HTTPD_CGI                  1
+
+#endif
